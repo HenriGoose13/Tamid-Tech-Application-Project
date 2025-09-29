762d942936f39e4a1607df6eb79f478d93c6f1d2
HenriGoose13 <henri.goussault@gmail.com>
2025-09-29 01:05:38 -0400

    Add files via upload

---
+++ tamid-app/backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+
+app.use(express.static(path.join(__dirname, "..", "frontend")));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "frontend", "index.html"))
+})
+
+// About page
+app.get("/abigailrosenfeld", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "abigail.html"));
+});
+
+// Contact page
+app.get("/mattheweichenholz", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "matthew.html"));
+});
+
+app.get("/remyeidelman", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "remy.html"));
+});
+
+app.listen(4000,() => {
+    console.log("Listening on port 4000")
+})
---
+++ tamid-app/frontend/script.js
@@ -0,0 +1,12 @@
+// script.js
+document.getElementById("abigailBtn").addEventListener("click", () => {
+  window.location.href = "/abigailrosenfeld";
+});
+
+document.getElementById("matthewBtn").addEventListener("click", () => {
+  window.location.href = "/mattheweichenholz";
+});
+
+document.getElementById("remyBtn").addEventListener("click", () => {
+  window.location.href = "/remyeidelman";
+});
